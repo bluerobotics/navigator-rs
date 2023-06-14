720f1b86073a1a4c1b13b89909b1ca4224d0ff6d
joaoantoniocardoso <joaoantoniocardoso@users.noreply.github.com>
2023-06-14 17:05:20 +0000

    deploy: afd106c904f795435a4cc19422d50929796e301d

--- implementors/core/iter/traits/marker/trait.FusedIterator.js
+++ implementors/core/iter/traits/marker/trait.FusedIterator.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"nix":[["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.69.0/core/iter/traits/marker/trait.FusedIterator.html\" title=\"trait core::iter::traits::marker::FusedIterator\">FusedIterator</a> for <a class=\"struct\" href=\"nix/sys/select/struct.Fds.html\" title=\"struct nix::sys::select::Fds\">Fds</a>&lt;'a&gt;"]]
+"nix":[["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.70.0/core/iter/traits/marker/trait.FusedIterator.html\" title=\"trait core::iter::traits::marker::FusedIterator\">FusedIterator</a> for <a class=\"struct\" href=\"nix/sys/select/struct.Fds.html\" title=\"struct nix::sys::select::Fds\">Fds</a>&lt;'a&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
