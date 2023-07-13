e9bf6d87ffabff5e0b901e19463d23022fc3038c
patrickelectric <patrickelectric@users.noreply.github.com>
2023-07-13 16:56:05 +0000

    deploy: 4e5fd87ecf3160dd059d106b73f9ad6f48c8e1b0

--- implementors/std/io/trait.Read.js
+++ implementors/std/io/trait.Read.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {
-"nix":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.70.0/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a> for <a class=\"struct\" href=\"nix/pty/struct.PtyMaster.html\" title=\"struct nix::pty::PtyMaster\">PtyMaster</a>"]],
-"serial_unix":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.70.0/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a> for <a class=\"struct\" href=\"serial_unix/struct.TTYPort.html\" title=\"struct serial_unix::TTYPort\">TTYPort</a>"]],
-"spidev":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.70.0/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a> for <a class=\"struct\" href=\"spidev/struct.Spidev.html\" title=\"struct spidev::Spidev\">Spidev</a>"]]
+"nix":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.71.0/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a> for <a class=\"struct\" href=\"nix/pty/struct.PtyMaster.html\" title=\"struct nix::pty::PtyMaster\">PtyMaster</a>"]],
+"serial_unix":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.71.0/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a> for <a class=\"struct\" href=\"serial_unix/struct.TTYPort.html\" title=\"struct serial_unix::TTYPort\">TTYPort</a>"]],
+"spidev":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.71.0/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a> for <a class=\"struct\" href=\"spidev/struct.Spidev.html\" title=\"struct spidev::Spidev\">Spidev</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
