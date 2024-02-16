997313abf3999ba8b8b464122d30558431e3beb3
patrickelectric <patrickelectric@users.noreply.github.com>
2024-02-16 16:32:04 +0000

    deploy: ffb99c3d4f22c1bcb544a832dd352594252e656c

---
+++ doc/trait.impl/embedded_hal/blocking/i2c/trait.Read.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"linux_embedded_hal":[["impl <a class=\"trait\" href=\"embedded_hal/blocking/i2c/trait.Read.html\" title=\"trait embedded_hal::blocking::i2c::Read\">Read</a> for <a class=\"struct\" href=\"linux_embedded_hal/struct.I2cdev.html\" title=\"struct linux_embedded_hal::I2cdev\">I2cdev</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ doc/type.impl/std/primitive.u16.js
@@ -0,0 +1,5 @@
+(function() {var type_impls = {
+"embedded_hal":[],
+"nix":[],
+"serde":[]
+};if (window.register_type_impls) {window.register_type_impls(type_impls);} else {window.pending_type_impls = type_impls;}})()
