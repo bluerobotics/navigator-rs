7c0fbd2c0c6251fc1c1f5298a4c9dcc49ab375e0
Patrick José Pereira <patrickelectric@gmail.com>
2024-11-22 14:10:19 -0300

    bench: Update to use new api
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,23 +1,15 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use navigator_rs::{AdcChannel, Navigator, PwmChannel, UserLed};
+use navigator_rs::{AdcChannel, Navigator, UserLed};
 
 fn navigator_benchmark(c: &mut Criterion) {
     #[macro_export]
     macro_rules! bench {
     ($bench_fn:ident($($arg:tt)*)) => {
         let mut nav = Navigator::new();
-        nav.init();
         c.bench_function(stringify!($bench_fn), |b| b.iter(|| nav.$bench_fn($($arg)*)));
     }}
 
-    // Navigator creation benchmark
-    c.bench_function("new", |b| b.iter(|| Navigator::new()));
-
-    // Benchmark Inputs
-    bench!(init());
-
     bench!(read_adc(AdcChannel::Ch0));
-    bench!(read_adc_all());
     bench!(read_all());
 
     bench!(read_accel());
@@ -27,17 +19,13 @@ fn navigator_benchmark(c: &mut Criterion) {
     bench!(read_pressure());
     bench!(read_temperature());
 
-    bench!(read_all());
+    bench!(read_adc_all());
 
     // Benchmark Outputs
     bench!(set_pwm_enable(false));
-    bench!(get_pwm_enable());
-    bench!(set_pwm_channel_value(PwmChannel::Ch1, 100));
-    bench!(set_pwm_freq_hz(60.0));
-    bench!(set_pwm_freq_prescale(100));
-
+    bench!(set_pwm_duty_cycle(0, 0.1));
+    bench!(set_pwm_frequency(60.0));
     bench!(set_neopixel(&[[0, 0, 0]]));
-
     bench!(set_led(UserLed::Led1, false));
     bench!(set_led_toggle(UserLed::Led1));
     bench!(get_led(UserLed::Led1));
