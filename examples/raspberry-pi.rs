292c4a38b7fc6a67ab2cc941ae77da9ed35b2d37
Patrick José Pereira <patrickelectric@gmail.com>
2024-11-27 09:17:36 -0300

    examples: raspberry-pi: Add RGB LEDs and multiple PWMs values
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- examples/raspberry-pi.rs
+++ examples/raspberry-pi.rs
@@ -2,6 +2,8 @@ use navigator_rs::Navigator;
 use std::thread::sleep;
 use std::time::Duration;
 
+use rand::seq::SliceRandom;
+
 fn main() {
     println!("Creating your navigator module!");
     let mut nav = Navigator::new();
@@ -10,8 +12,20 @@ fn main() {
     nav.set_pwm_enable(true);
     nav.set_pwm_frequency(60.0);
 
+    let colors = [
+        [255u8, 0, 0],
+        [0, 255, 0],
+        [0, 0, 255],
+        [255, 255, 0],
+        [0, 255, 255],
+        [255, 0, 255],
+    ];
+
     loop {
-        nav.set_duty_cycle_all(0.25);
-        sleep(Duration::from_millis(1000));
+        for value in [0.25, 0.5, 0.75, 0.5] {
+            nav.set_neopixel(&[*colors.choose(&mut rand::thread_rng()).unwrap()]);
+            nav.set_duty_cycle_all(value);
+            sleep(Duration::from_millis(1000));
+        }
     }
 }
