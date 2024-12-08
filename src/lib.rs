665616bf9a20d265d4877e5cde00505803b14343
Patrick José Pereira <patrickelectric@gmail.com>
2024-12-08 06:15:20 -0300

    lib: Add panic if neopixel does not exist
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -349,6 +349,14 @@ impl Navigator {
         }
     }
 
+    pub fn set_neopixel_rgbw(&mut self, colors: &[[u8; 4]]) {
+        if let Some(rgb_behaviour) = self.get_rgb_behaviour() {
+            rgb_behaviour.set_colors_rgbw(colors).unwrap();
+        } else {
+            panic!("No RGB controller available");
+        }
+    }
+
     pub fn read_all(&mut self) -> SensorData {
         SensorData {
             adc: self.read_adc_all(),
